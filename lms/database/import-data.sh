2da5d1e922e51a3973f572ae97452c7989fd969a
Bartosz Lachowicz <jendrekpb@gmail.com>
2022-04-27 13:24:27 +0200

    Change 50 retries to 5

--- lms/database/import-data.sh
+++ lms/database/import-data.sh
@@ -1,6 +1,6 @@
 #run the setup script to create the DB and the schema in the DB
 #do this in a loop because the timing for when the SQL instance is ready is indeterminate
-for i in {1..50};
+for i in {1..5};
 do
     /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Yukon900 -d master -i setup.sql
     if [ $? -eq 0 ]
